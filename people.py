738c7f4d7e26cd3cb39ada553b96b2bfa44b01fa
jinyan5 <SeeJY_94@hotmail.com>
2015-10-19 13:05:57 +0100

    added people

--- people.py
+++ people.py
@@ -1,12 +1,109 @@
 from items import *
 
+#chemistry student (dynamite)
 Toby = {
     "name": "Toby",
 
-    "description":
-    """""",
+    "description": 
+    """ A typical mad scienctist look with hair like Ablert Einstein,
+    who wears a lab coat 24/7. Toby is the blackmarket of chemicals
+    and explosives.""",
 
-    "speech": "",
+    "speech": "Oh hey there Yu, how's it going? Do you need anything?? *Wink wink*",
+
+    "items": [dynamite, chemicals, alcohol]
+
+    "take": False
+}
+
+#antisocial roommate comscience
+Jill = {
+    "name": "Jill",
+
+    "description":
+    """ Jill is as silent as silent night, doesn't really speak to anyone. 
+    She spends most of her time reading books and watching Friends and How I
+     Met Your Mother. Aside from that, she knows her stuff about computers.""",
+
+    "speech": "It's you again Yu, i would talk to you but i don't want to. So, bye!",
 
     "items": []
+
+    "take": False
 }
+
+#party people 1
+Eric = {
+    "name": "Eric",
+
+    "description":
+    """ If you look up party in a dictionary, you'd see his face.""",
+
+    "speech": "Everyday I'm shuffling!!!!",
+
+    "items": []
+
+    "take": False
+}
+
+
+#party people 2
+David = {
+    "name": "David",
+
+    "description":
+    """ David A.K.A the alcoholic. He basically drinks and
+    gets drunk every week, last person you would want to 
+    play drinking games with. """,
+
+    "speech": "Hmmmm??? Whhaaat?? *Hiccup* ",
+
+    "items": []
+
+    "take": False
+}
+
+#party people 3
+Sally = {
+    "name": "Sally",
+
+    "description":
+    """ Friendly, loves to socialize and meeting new people. Sometiems 
+    maybe a little too enthusiastic.""",
+
+    "speech": "HI YOU!!!!! COME JOIN THE PARTY!!!",
+
+    "items": []
+
+    "take": False
+}
+
+#guard to the party
+Maypac = {
+    "name": "Maypac",
+
+    "description":
+    """ Tough guy, big in size and looks like he could knock anyone out
+    with a single punch.""",
+
+    "speech": "Did you bring any drinks?",
+
+    "items": []
+
+    "take": False
+}
+
+#zombie
+Fluffy = {
+    "name": "Fluffy",
+
+    "description":
+    """Well, he's a zombie.""",
+
+    "speech": "Brrraaaaiinnnnssss, BBBRRaaaaIINNSss",
+
+    "items": []
+
+    "take": False
+}
+
